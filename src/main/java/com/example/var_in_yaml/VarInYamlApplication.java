422a140fbf5767816ab418b9871d5619dba1fef7
Masahiko Kamo <m-kamo@unext.jp>
2020-02-14 17:58:07 +0900

    starter-webを削除

--- src/main/java/com/example/var_in_yaml/VarInYamlApplication.java
+++ src/main/java/com/example/var_in_yaml/VarInYamlApplication.java
@@ -1,8 +1,11 @@
 package com.example.var_in_yaml;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @Slf4j
@@ -13,5 +16,12 @@ public class VarInYamlApplication {
 
     var fooProps = app.getBean(FooProperties.class);
     log.info("example.foo.bar={}", fooProps.getBar());
+  }
+
+  @Component
+  @ConfigurationProperties(prefix = "example.foo")
+  @Data
+  public static class FooProperties {
+    private String bar;
   }
 }
